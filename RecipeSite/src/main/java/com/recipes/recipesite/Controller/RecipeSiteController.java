32395116db2785154c9fceabb5d752ac04edeb7b
HyperionPanda <aidanghorn1@gmail.com>
2024-05-16 12:25:51 -0700

    Added AccountService. Added AccountRepository. Connected controller to service and service to repository through basic functionality.

--- RecipeSite/src/main/java/com/recipes/recipesite/Controller/RecipeSiteController.java
+++ RecipeSite/src/main/java/com/recipes/recipesite/Controller/RecipeSiteController.java
@@ -1,6 +1,8 @@
 package com.recipes.recipesite.Controller;
 
 import com.recipes.recipesite.Entity.Account;
+import com.recipes.recipesite.Service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 public class RecipeSiteController {
 
   //@Autowired services here
+  @Autowired
+  AccountService accountService;
 
 
 
@@ -21,8 +25,10 @@ public class RecipeSiteController {
 
 
   @PostMapping(value = "newAccount")
-  public ResponseEntity<Account> addAccount(@RequestBody Account newAccount){
-      return ResponseEntity.ok(newAccount);
+  public ResponseEntity<Integer> addAccount(@RequestBody Account newAccount){
+      Integer result = accountService.addAccount(newAccount);
+      //test result if 1 or 0?
+      return ResponseEntity.ok(result);
   }
 
 }
---
+++ RecipeSite/src/main/java/com/recipes/recipesite/Service/AccountService.java
@@ -0,0 +1,23 @@
+package com.recipes.recipesite.Service;
+
+import com.recipes.recipesite.Entity.Account;
+import com.recipes.recipesite.Repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+  @Autowired
+  private AccountRepository accountRepository;
+
+  //use @Transactional ?
+  public Integer addAccount(Account account) {
+    if (account != null){
+      Account test = accountRepository.save(account);
+      if (test != null) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+}
