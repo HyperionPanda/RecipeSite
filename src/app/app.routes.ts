86b6cdfb57b5842c3eafada5765d4fbd8958cd61
HyperionPanda <aidanghorn1@gmail.com>
2024-05-19 20:36:41 -0700

    create-account created and attached to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
 
-export const routes: Routes = [{path:'login', component:LoginComponent}, {path: '', redirectTo: '/login', pathMatch: 'full'}];
+export const routes: Routes = [
+    
+    {path:'login', component:LoginComponent},
+    {path:'newAccount', component:CreateAccountComponent}, 
+    {path: '', redirectTo: '/login', pathMatch: 'full'}
+
+
+
+
+];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,20 +1,24 @@
 import { Component, ElementRef } from '@angular/core';
+import { CreateAccountComponent } from '../create-account/create-account.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CreateAccountComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  constructor(private el : ElementRef){}
+  constructor(private el : ElementRef, private router: Router){}
 
   verifyAccount(){
 
     const username = this.el.nativeElement.querySelector("#username").value;
     const password = this.el.nativeElement.querySelector("#password").value;
+
+    
     
     /* Call to back-end to verify username and password */
     try{
@@ -22,6 +26,12 @@ export class LoginComponent {
     }catch(e){
       alert("Username/Password given does not match known account")
     }
+  }
+
+  createAccount(){
+    this.router.navigate(['/newAccount'])
+      .then(success => console.log('navigation success?' , success))
+      .catch(console.error); 
   }
 
 }
