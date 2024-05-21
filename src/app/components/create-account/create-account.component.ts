651e2e8b2880b909f380f4461a1991d28820c991
HyperionPanda <aidanghorn1@gmail.com>
2024-05-21 13:44:26 -0700

    added account creation checks and styling

--- src/app/components/create-account/create-account.component.ts
+++ src/app/components/create-account/create-account.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-create-account',
@@ -7,6 +7,47 @@ import { Component } from '@angular/core';
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.css'
 })
-export class CreateAccountComponent {
+export class CreateAccountComponent implements AfterViewInit {
+
+  constructor(private renderer : Renderer2,private element : ElementRef){}
+
+  ngAfterViewInit(): void {
+    
+      const fullPage = this.element.nativeElement.ownerDocument.body;
+      this.renderer.setStyle(fullPage,"backgroundColor","blue");
+
+  }
+
+  validateFields(){
+
+    const username = this.element.nativeElement.querySelector("#username");
+    const password = this.element.nativeElement.querySelector("#password");
+    const otherpassword = this.element.nativeElement.querySelector("#otherpassword");
+
+    if(username.value.length < 5){
+      alert("Username must be longer than 5 letters");
+      return;
+    }else if (username.value.length > 15){
+      alert("Username must be shorter than 15 letters");
+      return;
+    }
+
+    if(password.value.length < 5){
+      alert("Password must be longer than 5 letters");
+      return;
+    }else if (password.value.length > 15){
+      alert("Password must be shorter than 15 letters");
+      return;
+    }else if(password.value != otherpassword.value){
+      alert("The passwords in each field must be the same");
+      return;
+    }
+
+    this.makeAccount();
+  }
+  makeAccount(){
+    alert("clicked");
+
+  }
 
 }
