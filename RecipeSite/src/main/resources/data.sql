6301069049752653bf7b13ce216fc53a037106c6
HyperionPanda <aidanghorn1@gmail.com>
2024-05-28 21:43:26 -0700

    Added Recipe to data.sql.

--- RecipeSite/src/main/resources/data.sql
+++ RecipeSite/src/main/resources/data.sql
@@ -1,7 +1,16 @@
+//probably remove this later, shouldn't drop tables with stuff stored
 DROP TABLE IF EXISTS Account;
+DROP TABLE IF EXISTS Recipe;
 
 CREATE TABLE Account(
   id int NOT NULL PRIMARY KEY auto_increment,
   username varchar(30) NOT NULL,
   password varchar(30) NOT NULL
+);
+
+CREATE TABLE Recipe(
+  recipeId int NOT NULL PRIMARY KEY auto_increment,
+  userId int NOT NULL,
+  ingredients varchar(2000),
+  cookingSteps varchar(5000)
 );
