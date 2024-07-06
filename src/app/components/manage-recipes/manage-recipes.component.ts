4337b5cacda9912e1f609104bb608988759b340d
HyperionPanda <aidanghorn1@gmail.com>
2024-07-05 22:32:49 -0700

    Added example recipes. Created conatiner grid for manage recipes and allowed for recipes to actually be read into the page

--- src/app/components/manage-recipes/manage-recipes.component.ts
+++ src/app/components/manage-recipes/manage-recipes.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Recipe } from '../../models/Recipe';
 import { HttpClient } from '@angular/common/http';
+import { exampleRecipe1,exampleRecipe2,exampleRecipe3 } from '../../utils/mockData';
 @Component({
   selector: 'app-manage-recipes',
   standalone: true,
@@ -9,14 +10,20 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './manage-recipes.component.css'
 })
 export class ManageRecipesComponent implements OnInit {
+
   recipes : Recipe[];
+  testRecipes : Recipe[];
 
-  constructor(private http : HttpClient){this.recipes = []}
-
-  ngOnInit(): void {
-      this.getRecipes();
+  constructor(private http : HttpClient, private el : ElementRef){
+    this.recipes = [];
+    this.testRecipes = [exampleRecipe1,exampleRecipe2,exampleRecipe3];
   }
 
+  
+  ngOnInit(): void {
+      //this.getRecipes();
+  }
+  /*
   getRecipes(){
     const accountName = sessionStorage.getItem("account");
     const json = {
@@ -39,9 +46,34 @@ export class ManageRecipesComponent implements OnInit {
       alert("Log in to access your recipes")
     }
   }
+  */
+  viewRecipe(index : Number){
+    const recipeButton = this.el.nativeElement.querySelector("#recipe"+index);
+    let recipe;
 
-  viewRecipe(){
-    console.log("view Recipe");
+    //call database to find recipe with specific name
+    
+    /*
+    const json = {
+      method: "POST",
+      body: JSON.stringify(recipe.value),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    }
+    if(recipeButton.value != ""){
+      try{
+        this.http.post<Recipe>("http://localhost:8080/findRecipe",json)
+          .subscribe(value => recipe);
+
+
+      }catch(e){
+        console.log("Some error occured");
+        console.log("Error : "+e);
+      }
+
+    }else{
+      alert("Something went wrong with your saved Recipe, Please try again later")
+    }
+    */
   }
 
 }
---
+++ src/app/utils/mockData.ts
@@ -0,0 +1,26 @@
+import { Account } from "../models/Account";
+import { Recipe } from "../models/Recipe";
+
+export const exampleRecipe1 : Recipe = {
+    recipeId : 1,
+    userId : 1,
+    name : "Example 1",
+    ingredients : "Some Ingredients",
+    cookingSteps : "Some Steps"
+}
+
+export const exampleRecipe2 : Recipe = {
+    recipeId : 2,
+    userId : 1,
+    name : "Example 2",
+    ingredients : "Some Ingredients Part 2",
+    cookingSteps : "Some Steps Part 2"
+}
+
+export const exampleRecipe3 : Recipe = {
+    recipeId : 3,
+    userId : 1,
+    name : "Example 3",
+    ingredients : "Some Ingredients  Part 3",
+    cookingSteps : "Some Steps  Part 3"
+}
